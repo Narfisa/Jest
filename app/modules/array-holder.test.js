6bee92ac2e60be0259f20873cd87fd20cdc08fb1
Valya <valya.okulova@mail.ru>
2020-04-01 10:59:29 +0700

    re-install jest + deleted anonymous funcs

--- app/modules/array-holder.test.js
+++ app/modules/array-holder.test.js
@@ -12,26 +12,14 @@ test("get unexisting element", () => {
 test("get element by index", () => {
     let arrayHolder = new ArrayHolder()
     arrayHolder.set(array)
-    expect(() => {
-        arrayHolder.getElementByIndex(0)
-    }).toBe(array[0])
+    expect(arrayHolder.getElementByIndex(0)).toBe(array[0])
 })
 
 test("instance created", () => {
     let arrayHolder = new ArrayHolder()
-    expect(() => {
-        arrayHolder.average()
-    }).toBe(0)
-    expect(() => {
-        arrayHolder.geometricMean()
-    }).toBe(0)
-    expect(() => {
-        arrayHolder.sum()
-    }).toBe(0)
-    expect(() => {
-        arrayHolder.max()
-    }).toBe(0)
-    expect(() => {
-        arrayHolder.min()
-    }).toBe(0)
+    expect(arrayHolder.average()).toBe(0)
+    expect(arrayHolder.geometricMean()).toBe(0)
+    expect(arrayHolder.sum()).toBe(0)
+    expect(arrayHolder.max()).toBe(0)
+    expect(arrayHolder.min()).toBe(0)
 })
