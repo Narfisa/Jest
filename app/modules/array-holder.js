28c68f4610a7de2be068bf457074e16c965f7c91
Valya <valya.okulova@mail.ru>
2020-04-01 10:46:00 +0700

    changed conditions for empty array

--- app/modules/array-holder.js
+++ app/modules/array-holder.js
@@ -17,49 +17,49 @@ export default class ArrayHolder {
     }
 
     getElementByIndex(index) {
-        if (typeof(this.arrayStore[index]) === "undefined") {
+        if (this.arrayStore.length == 0) {
             throw new Error("Unexisting element. Cannot get element with index was given")
         }
         return this.arrayStore[index];
     }
 
     removeElementByIndex(index) {
-        if (typeof(this.arrayStore[index]) === "undefined") {
+        if (this.arrayStore.length == 0) {
             throw new Error("Array is epmty. Cannot delete element from epmty array")
         }
         return this.arrayStore.slice(index, 1);
     }
 
     sum() {
-        if (this.arrayStore === []) {
+        if (this.arrayStore.length == 0) {
             return 0
         }
         return this.arrayStore.reduce((a, b) => a + b);
     }
 
     average() {
-        if (this.arrayStore === []) {
+        if (this.arrayStore.length == 0) {
             return 0
         }
         return this.sum() / this.arrayStore.length;
     }
 
     geometricMean() {
-        if (this.arrayStore === []) {
+        if (this.arrayStore.length == 0) {
             return 0
         }
         return Math.pow(this.sum(), 1 / this.arrayStore.length);
     }
 
     min() {
-        if (this.arrayStore === []) {
+        if (this.arrayStore.length == 0) {
             return 0
         }
         return Math.min(...this.arrayStore);
     }
 
     max() {
-        if (this.arrayStore === []) {
+        if (this.arrayStore.length == 0) {
             return 0
         }
         return Math.max(...this.arrayStore);
