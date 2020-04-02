558e8866a357a26b88969e21aeaff8f8f94bced1
Valya <valya.okulova@mail.ru>
2020-04-02 16:13:46 +0700

    added coverage and new checks

--- app/modules/array-holder.test.js
+++ app/modules/array-holder.test.js
@@ -22,16 +22,23 @@ test("instance created", () => {
     expect(arrayHolder.sum()).toBe(0)
     expect(arrayHolder.max()).toBe(0)
     expect(arrayHolder.min()).toBe(0)
+    expect(() => {
+        arrayHolder.removeElementByIndex(0)
+    }).toThrow()
 })
 
 test("instance created and adding 1 value", () => {
     let arrayHolder = new ArrayHolder()
-    arrayHolder.set([1])
+    let arr = [1]
+    arrayHolder.set(arr)
     expect(arrayHolder.average()).toBe(1)
     expect(arrayHolder.geometricMean()).toBe(1)
     expect(arrayHolder.sum()).toBe(1)
     expect(arrayHolder.max()).toBe(1)
     expect(arrayHolder.min()).toBe(1)
+    expect(arrayHolder.get()).toBe(arr)
+    arrayHolder.clear()
+    expect(arrayHolder.mul()).toBe(0)
 })
 
 test("Array=[1,2,3]. Deleted last value.", () => {
